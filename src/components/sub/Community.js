a7d4900df3e13d1687af7f12b29fdc3129e32530
haglass <haglass@hanmail.net>
2023-02-01 10:35:18 +0900

    useEffect 및 useRef 를 활용한 모션 작업

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../common/Layout";
 const Community = () => {
+  // jascript 코드자리
+  // 컴포넌트가 마운트,업데이트(state변경),언마운트 시
+  // 하고싶은일을 작성하는 Hook : userEffect
+  useEffect(()=>{
+    console.log("mount : 컴포넌트 생성")
+    // axios,html요소 선택하는 작업 많이 함
+    return ()=>{
+      console.log("unmount : 컴포넌트 제거");
+    }
+  },[]);
+  // jsx코드 자리 :주의 사항 return()
   return <Layout title={"Community"}>Community</Layout>;
 };
 
