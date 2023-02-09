f4f5e49cbacea6dff58027cff5001ae09e99d856
haglass <haglass@hanmail.net>
2023-02-09 11:59:23 +0900

    리셋추가

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -177,7 +177,11 @@ const Join = () => {
 
     return errs;
   };
-
+  // 데이터 조기화
+  const handleReset = () => {
+    setVal(initVal);
+    setErr({});
+  };
   // 디버깅용
   useEffect(() => {
     console.log(val);
@@ -442,7 +446,7 @@ const Join = () => {
               <tr>
                 <th colSpan="2">
                   {/* <button type="button">전송</button> */}
-                  <input type="reset" value="RESET" />
+                  <input type="reset" onClick={handleReset} value="RESET" />
                   <input type="submit" value="SUBMIT" />
                 </th>
               </tr>
